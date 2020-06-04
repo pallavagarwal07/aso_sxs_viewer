5755a8bee2c94749bb0ba46dd9e9412a7041155f
Leshna Balara <leshnabalara2001@gmail.com>
2020-06-04 10:37:22 +0530

    Improve formatting

--- command/command_test.go
+++ command/command_test.go
@@ -1,7 +1,7 @@
 package command
 
 import (
-	"fmt"
+	"errors"
 	"os"
 	"testing"
 	"time"
@@ -26,52 +26,45 @@ func TestExecuteProgram(t *testing.T) {
 		t.Errorf("Encountered error %s by ExecuteProgram()", err.Error())
 	}
 
+	if err := programstate.Stderr(); len(err) != 0 {
+		t.Errorf("Returned error %s by StderrNonBlocking()", string(err))
+	}
+
 	want := "Hello\n"
-	got := testWaitOutput(programstate.Stdout, want)
-
-	serr := programstate.Stderr()
-	if len(serr) != 0 {
-		fmt.Println("Returned error by StderrNonBlocking()", string(serr))
+	if err := testWaitOutput(programstate.Stdout, want); err != nil {
+		t.Errorf(err.Error())
 	}
 
-	if got != want {
-		t.Errorf("Got text = %q, want %q", got, want)
-	}
-
-	err = programstate.Command.Process.Signal(os.Interrupt)
-	if err != nil {
+	if err := programstate.Command.Process.Signal(os.Interrupt); err != nil {
 		t.Errorf("Encountered error %s while sending interrupt signal", err.Error())
 	}
 
-	want = "Hello\nWorld\n"
-	got = testWaitOutput(programstate.Stdout, want)
-
-	serr = programstate.Stderr()
-	if len(serr) != 0 {
-		fmt.Println("Returned error by StderrNonBlocking()", string(serr))
+	if err := programstate.Stderr(); len(err) != 0 {
+		t.Errorf("Returned error %s by StderrNonBlocking()", string(err))
 	}
 
-	if got != want {
-		t.Errorf("Got text = %q, want %q", got, want)
+	want = "Hello\nWorld\n"
+	if err := testWaitOutput(programstate.Stdout, want); err != nil {
+		t.Errorf(err.Error())
 	}
 }
 
 func testErrorHandler(p *ProgramState, err error) error {
-	fmt.Println("Test_error_handler", err)
+	t.Errorf("Invoked testErrorHandler with error %s", err.Error())
 	return err
 }
 
-func testWaitOutput(getter func() []byte, want string) string {
+func testWaitOutput(getter func() []byte, want string) error {
 	timeout := time.After(1 * time.Second)
 	result := ""
 	for result != want {
 		select {
 		case <-timeout:
-			return result
+			return errors.New("Got text = " + result + " want " + want)
 		default:
 		}
 		result = string(getter())
 	}
 
-	return result
+	return nil
 }

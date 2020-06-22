b5ee1def6704b06eb9c78db9a1eeaf0b314ef4eb
Leshna Balara <leshnabalara2001@gmail.com>
2020-06-23 04:09:06 +0530

    Create mock chrome server using log file in chrometool_test.go. Remove dependencies on experimental Chrome DevTools features for click and scroll Automatically assign port for the mock chrome. Use flags to determine ports and whether to run the test http server

--- chrometool/chrometool.go
+++ chrometool/chrometool.go
@@ -9,6 +9,11 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
+const (
+	windowDimensionsJS = `[window.innerWidth, window.innerHeight];`
+	windowScrollJS     = `window.scrollBy(%d, %d);`
+)
+
 // SendKeysToNthElement is an element query action that synthesizes the key up, char, and down
 // events as needed for the runes in v, sending them to the nth element node
 // matching the selector.
@@ -55,27 +60,81 @@ func ClickNthElement(sel interface{}, n int, opts ...chromedp.QueryOption) chrom
 // viewport.
 func MouseClickNode(n *cdp.Node, opts ...chromedp.MouseOption) chromedp.MouseAction {
 	return chromedp.ActionFunc(func(ctx context.Context) error {
-		dom.ScrollIntoViewIfNeeded().WithNodeID(n.NodeID).Do(ctx)
+		var windowDim []float64
+		if err := chromedp.Evaluate(windowDimensionsJS, &windowDim).Do(ctx); err != nil {
+			return err
+		}
 
-		boxes, err := dom.GetContentQuads().WithNodeID(n.NodeID).Do(ctx)
+		box, err := dom.GetBoxModel().WithNodeID(n.NodeID).Do(ctx)
 		if err != nil {
 			return err
 		}
-		content := boxes[0]
 
-		c := len(content)
-		if c%2 != 0 || c < 1 {
-			return chromedp.ErrInvalidDimensions
+		x, y, err := EvalNodeCenter(box)
+		if err != nil {
+			return err
 		}
 
-		var x, y float64
-		for i := 0; i < c; i += 2 {
-			x += content[i]
-			y += content[i+1]
+		if x > windowDim[0] || x < 0 || y > windowDim[1] || y < 0 {
+			if err := ScrollNodeIntoView(ctx, n, x, y, windowDim, box.Border); err != nil {
+				return err
+			}
+			box, err := dom.GetBoxModel().WithNodeID(n.NodeID).Do(ctx)
+			if err != nil {
+				return err
+			}
+
+			x, y, err = EvalNodeCenter(box)
+			if err != nil {
+				return err
+			}
 		}
-		x /= float64(c / 2)
-		y /= float64(c / 2)
 
 		return chromedp.MouseClickXY(x, y, opts...).Do(ctx)
 	})
+}
+
+// ScrollNodeIntoView scrolles the window if the node is not within the window's
+// viewport.
+func ScrollNodeIntoView(ctx context.Context, n *cdp.Node, x float64, y float64, windowDim []float64, box []float64) error {
+	var xshift, yshift int
+	if x < 0 {
+		xshift = int(box[0]) - 1
+	} else if x > windowDim[0] {
+		xshift = int(box[2]-windowDim[0]) + 1
+	}
+
+	if y < 0 {
+		yshift = int(box[1]) - 1
+	} else if y > windowDim[1] {
+		yshift = int(box[5]-windowDim[1]) + 1
+	}
+
+	var res interface{}
+	if err := chromedp.Evaluate(fmt.Sprintf(windowScrollJS, xshift, yshift), &res).Do(ctx); err != nil {
+		if err.Error() == "encountered an undefined value" {
+			err = nil
+		}
+		return err
+	}
+	return nil
+}
+
+// EvalNodeCenter evaluates a node's central coordinates and returns x, y
+func EvalNodeCenter(box *dom.BoxModel) (float64, float64, error) {
+	content := box.Border
+	c := len(content)
+	if c%2 != 0 || c < 1 {
+		return 0, 0, chromedp.ErrInvalidDimensions
+	}
+
+	var x, y float64
+	for i := 0; i < c; i += 2 {
+		x += content[i]
+		y += content[i+1]
+	}
+	x /= float64(c / 2)
+	y /= float64(c / 2)
+
+	return x, y, nil
 }

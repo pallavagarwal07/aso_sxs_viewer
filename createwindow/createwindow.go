4f9d2ff0e56229689cdea14a457fa921e5174725
ishika-b <ishikab@google.com>
2020-07-02 03:52:16 +0530

    added Sessions struct

--- createwindow/createwindow.go
+++ createwindow/createwindow.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"os"
+	"runtime"
 
 	"sync"
 
@@ -11,47 +16,83 @@ import (
 
 	"github.com/chromedp/chromedp"
 	"github.com/jezek/xgb"
+	"github.com/jezek/xgb/randr"
 	"github.com/jezek/xgb/xproto"
 )
 
-const WINDOWHEIGHT = 1400
-const WINDOWWIDTH = 2000
-const CHROMECONNTIMEOUT = 30
+const (
+	windowHeight      = 1400
+	windowWidth       = 2000
+	chromeConnTimeout = 30
+)
+
+// For everything that needs to use program list
+type Session struct {
+	windowList []Quitter
+	lock       sync.Mutex
+}
+
+// CreateChromeWindow opens a Chrome browser session
+func (s *Session) CreateChromeWindow(cmd command.ExternalCommand, ctxCh chan context.Context, cmdErrorHandler func(p *command.ProgramState, err error) error) error {
+	programstate, err := command.ExecuteProgram(cmd, cmdErrorHandler)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	ctx, err := establishChromeConnection(programstate, chromeConnTimeout)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	ctxCh <- ctx
+	s.appendWindowList(ChromeWindow{programstate})
+
+	return nil
+}
+
+func (s *Session) appendWindowList(newWindow Quitter) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.windowList = append(s.windowList, newWindow)
+}
+
+func (s *Session) getWindowList() []Quitter {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	programs := make([]Quitter, len(s.windowList))
+	copy(programs, s.windowList)
+	return programs
+}
 
 // Layout has the x , y coordinates of top left corner and width and height of window
 type Layout struct {
-	x, y, w, h int
+	x, y uint32
+	w, h uint16
 }
 
-//Quitters has method quit that closes that window and kills that program
-type Quitters interface {
+// Quitters has method quit that closes that window and kills that program
+type Quitter interface {
 	Quit()
 	ToClose() bool
 }
 
-//InputWindow is struct to hold information about the input window
+type ChromeWindow struct {
+	*command.ProgramState
+}
+
 type InputWindow struct {
 	Wid  xproto.Window
 	Conn *xgb.Conn
 }
 
-//QuitStruct has the slice of quittes and the lock
-type QuitStruct struct {
-	Quitters []Quitters
-	lock     sync.Mutex
-}
-
-//ChromeWindow is struct to hold information about Chrome browser sessions
-type ChromeWindow struct {
-	*command.ProgramState
-}
-
-//Quit method to close the Chrome browser sessions
+// Quit method to close the Chrome browser sessions
 func (p ChromeWindow) Quit() {
 	p.Command.Process.Kill()
 }
 
-//ToClose method checks whether ChromeWindow needs to be closed
+// ToClose method checks whether ChromeWindow needs to be closed
 func (p ChromeWindow) ToClose() bool {
 	return p.IsRunning()
 }
@@ -66,32 +107,74 @@ func (p *InputWindow) ToClose() bool {
 	return true
 }
 
-// tracks errors for execute program
-func cmdErrorHandler(p *command.ProgramState, err error) error {
-	fmt.Println(err)
-	return err
-}
-
-//ForceQuit closes everything
-func ForceQuit(a *QuitStruct) {
-
-	a.lock.Lock()
-	defer a.lock.Unlock()
+// ForceQuit closes all windows
+func (s *Session) ForceQuit() {
+	programs := s.getWindowList()
 
 	fmt.Println("starting force quit")
 
-	for _, q := range a.Quitters {
+	for _, q := range programs {
 		if q.ToClose() == true {
 			q.Quit() // will be quitting the other open Chrome Windows
 		}
 	}
+}
 
+// Setup opens all windows and establishes connection with the x server
+func Setup(n int, ctxCh chan context.Context) (*xgb.Conn, xproto.Window, error) {
+	debuggingport := 9222
+	var displayString string
+
+	var session Session
+	cmdErrorHandler := func(p *command.ProgramState, err error) error {
+		if err != nil {
+			fmt.Println("returned error %s, calling force quit", err.Error())
+		}
+		session.ForceQuit()
+		return err
+	}
+
+	if runtime.GOOS != "darwin" {
+		displayNumber := 1000 + rand.Intn(9999-1000+1)
+		displayString := fmt.Sprintf(":%d", displayNumber)
+		var xephyrLayout Layout
+		xephyrLayout.h, xephyrLayout.w = DefaultXephyrSize()
+		if err := session.CreateXephyrWindow(xephyrLayout, n, cmdErrorHandler); err != nil {
+			return nil, 0, err
+		}
+	}
+
+	X, screenInfo, err := Newconn(displayString)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	chromeLayouts, inputWindowLayout := WindowsLayout(screenInfo, n)
+
+	for i := 1; i <= n; i++ {
+		cmd := ChromeCommand(chromeLayouts[i-1], fmt.Sprintf("%s/.aso_sxs_viewer/profiles/dir%d", os.Getenv("HOME"), i), displayString, debuggingport+i)
+		go session.CreateChromeWindow(cmd, ctxCh, cmdErrorHandler)
+	}
+
+	return session.CreateInputWindow(inputWindowLayout, X, screenInfo)
+}
+
+func Newconn(displayString string) (*xgb.Conn, *xproto.ScreenInfo, error) {
+	X, err := xgb.NewConnDisplay(displayString)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	setup := xproto.Setup(X)
+	screenInfo := setup.DefaultScreen(X)
+
+	return X, screenInfo, nil
 }
 
 func establishChromeConnection(programState *command.ProgramState, timeout int) (context.Context, error) {
 	wsURL, err := command.WsURL(programState, timeout)
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("Could not connect to the chrome window. Encountered error %s", err.Error()))
+		return nil, fmt.Errorf("Could not connect to the chrome window. Encountered error %s", err.Error())
 	}
 
 	if wsURL == "" {
@@ -106,22 +189,53 @@ func establishChromeConnection(programState *command.ProgramState, timeout int) 
 	return ctx, nil
 }
 
-// DefaultWindowsLayout stores window size and position
-func DefaultWindowsLayout(screenInfo *xproto.ScreenInfo) (chromewindow1, chromewindow2, inputwindow Layout) {
+func DefaultXephyrSize() (height, width uint16) {
+	height, width = 900, 1600 // Sensible defaults in case the below fails.
 
-	heightScreen := screenInfo.HeightInPixels
-	widthScreen := screenInfo.WidthInPixels
+	X, err := xgb.NewConn()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if err := randr.Init(X); err != nil {
+		log.Println(err)
+		return
+	}
 
-	chromewindow1.h = int(heightScreen - 150)
-	chromewindow1.w = int(widthScreen / 2)
-	chromewindow2.h = int(heightScreen - 150)
-	chromewindow2.w = int(widthScreen / 2)
-	inputwindow.h = 100
-	inputwindow.w = int(widthScreen)
+	screens, err := randr.GetScreenResourcesCurrent(X, xproto.Setup(X).DefaultScreen(X).Root).Reply()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-	chromewindow2.x = int(widthScreen / 2)
-	inputwindow.y = int(heightScreen - 150)
+	crtc, err := randr.GetCrtcInfo(X, screens.Crtcs[0], xproto.TimeCurrentTime).Reply()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	return uint16(0.8 * float64(crtc.Height)), uint16(0.8 * float64(crtc.Width))
+}
 
-	return chromewindow1, chromewindow2, inputwindow
+// WindowsLayout stores window size and position
+func WindowsLayout(screenInfo *xproto.ScreenInfo, n int) (chromeLayouts []Layout, inputwindow Layout) {
+	heightScreen, widthScreen := 0.8*screenInfo.HeightInPixels, screenInfo.WidthInPixels
+	inputwindow.h, inputwindow.w = uint16(0.2*screenInfo.HeightInPixels), uint16(widthScreen)
+	inputwindow.y = uint32(heightScreen)
 
+	rows := int(n/4) + 1
+	columns := math.Ceil(n / rows)
+
+	var temp Layout
+	temp.h = uint16(heightScreen / rows)
+	temp.w = uint16(widthScreen / columns)
+
+	for i, r := 0, rows; r > 0; r-- {
+		temp.y = r * temp.h
+		for c := columns; c > 0 && i < n; c-- {
+			temp.x = c * temp.w
+			chromeLayouts = append(chromeLayouts, temp)
+			i++
+		}
+	}
+	return chromeLayouts, inputwindow
 }
--- createwindow/window_darwin.go
+++ createwindow/window_darwin.go
@@ -3,52 +3,14 @@
 package createwindow
 
 import (
-	"context"
 	"fmt"
-	"log"
-	"time"
 
 	"../command"
-	"github.com/jezek/xgb"
-	"github.com/jezek/xgb/xproto"
 )
 
-// Setup opens all the windows and establishes connection with the X server
-func Setup(ctxCh chan context.Context) (*xgb.Conn, xproto.Window, *QuitStruct, error) {
-	q := new(QuitStruct)
-	X, screenInfo, err := Newconn()
-	if err != nil {
-		return nil, 0, nil, err
-	}
-
-	chromewindow1, chromewindow2, inputwindow := DefaultWindowsLayout(screenInfo)
-
-	debuggingport1 := 9222
-	debuggingport2 := 9223
-
-	go CreateChromeWindow(chromewindow1, "/tmp/aso_sxs_viewer/dir1", ForceQuit, X, screenInfo, q, debuggingport1, ctxCh)
-	go CreateChromeWindow(chromewindow2, "/tmp/aso_sxs_viewer/dir2", ForceQuit, X, screenInfo, q, debuggingport2, ctxCh)
-	return CreateInputWindow(inputwindow, X, screenInfo, q)
-}
-
-// Newconn establishes connection with XQuartz
-func Newconn() (*xgb.Conn, *xproto.ScreenInfo, error) {
-	X, err := xgb.NewConn()
-
-	if err != nil {
-		return nil, nil, err
-	}
-
-	setup := xproto.Setup(X)
-	screenInfo := setup.DefaultScreen(X)
-
-	return X, screenInfo, nil
-}
-
-// CreateChromeWindow opens a Chrome browser session
-func CreateChromeWindow(layout Layout, userdatadir string, quitfunc func(*QuitStruct),
-	X *xgb.Conn, screenInfo *xproto.ScreenInfo, a *QuitStruct, debuggingport int, ctxCh chan context.Context) error {
-
+// PopulateCommand will take structured data after config file is implemented
+func ChromeCommand(layout Layout, userdatadir, display string,
+	debuggingPort int) command.ExternalCommand {
 	cmd := command.ExternalCommand{
 		Path: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
 		Arg: []string{"--user-data-dir=" + userdatadir,
@@ -58,28 +20,5 @@ func CreateChromeWindow(layout Layout, userdatadir string, quitfunc func(*QuitSt
 			fmt.Sprintf("--remote-debugging-port=%d", debuggingport),
 		},
 	}
-
-	programstate, err := command.ExecuteProgram(cmd, cmdErrorHandler)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	ctx, err := establishChromeConnection(programstate, CHROMECONNTIMEOUT)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	ctxCh <- ctx
-	a.Quitters = append(a.Quitters, ChromeWindow{programstate})
-
-	for {
-		time.Sleep(10 * time.Millisecond)
-		if programstate.IsRunning() == false {
-			fmt.Println("chrome closed- calling force quit")
-			quitfunc(a)
-			return nil
-		}
-	}
+	return cmd
 }

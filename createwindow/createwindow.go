b684f6e8fd29e1a8dea58340bef86f6277e41430
ishika-b <ishikab@google.com>
2020-07-02 22:40:46 +0530

    removed Quitters slice, layout upto 4 chrome windows

--- createwindow/createwindow.go
+++ createwindow/createwindow.go
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"log"
 	"math"
-	"math/rand"
 	"os"
-	"runtime"
 
 	"sync"
 
@@ -16,7 +14,6 @@ import (
 
 	"github.com/chromedp/chromedp"
 	"github.com/jezek/xgb"
-	"github.com/jezek/xgb/randr"
 	"github.com/jezek/xgb/xproto"
 )
 
@@ -26,56 +23,13 @@ const (
 	chromeConnTimeout = 30
 )
 
-// For everything that needs to use program list
+//Session contains all information that will be needed by main
 type Session struct {
-	windowList []Quitter
-	lock       sync.Mutex
-}
-
-// CreateChromeWindow opens a Chrome browser session
-func (s *Session) CreateChromeWindow(cmd command.ExternalCommand, ctxCh chan context.Context, cmdErrorHandler func(p *command.ProgramState, err error) error) error {
-	programstate, err := command.ExecuteProgram(cmd, cmdErrorHandler)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	ctx, err := establishChromeConnection(programstate, chromeConnTimeout)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	ctxCh <- ctx
-	s.appendWindowList(ChromeWindow{programstate})
-
-	return nil
-}
-
-func (s *Session) appendWindowList(newWindow Quitter) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	s.windowList = append(s.windowList, newWindow)
-}
-
-func (s *Session) getWindowList() []Quitter {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	programs := make([]Quitter, len(s.windowList))
-	copy(programs, s.windowList)
-	return programs
-}
-
-// Layout has the x , y coordinates of top left corner and width and height of window
-type Layout struct {
-	x, y uint32
-	w, h uint16
-}
-
-// Quitters has method quit that closes that window and kills that program
-type Quitter interface {
-	Quit()
-	ToClose() bool
+	lock        sync.Mutex
+	X           *xgb.Conn
+	BrowserList []context.Context
+	InputWin    InputWindow
+	chromeList  []ChromeWindow
 }
 
 type ChromeWindow struct {
@@ -84,7 +38,21 @@ type ChromeWindow struct {
 
 type InputWindow struct {
 	Wid  xproto.Window
-	Conn *xgb.Conn
+	Conn *xgb.Conn // see when this can be replaced with Session.X
+}
+
+func (s *Session) appendChromeList(chromeWin ChromeWindow) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.chromeList = append(s.chromeList, chromeWin)
+}
+
+func (s *Session) getChromeList() []ChromeWindow {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	programs := make([]ChromeWindow, len(s.chromeList))
+	copy(programs, s.chromeList)
+	return programs
 }
 
 // Quit method to close the Chrome browser sessions
@@ -107,25 +75,67 @@ func (p *InputWindow) ToClose() bool {
 	return true
 }
 
-// ForceQuit closes all windows
-func (s *Session) ForceQuit() {
-	programs := s.getWindowList()
+// CreateChromeWindow opens a Chrome browser session
+func (s *Session) CreateChromeWindow(cmd command.ExternalCommand, ctxCh chan context.Context,
+	cmdErrorHandler func(p *command.ProgramState, err error) error) error {
+	programstate, err := command.ExecuteProgram(cmd, cmdErrorHandler)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 
-	fmt.Println("starting force quit")
+	ctx, err := establishChromeConnection(programstate, chromeConnTimeout)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 
-	for _, q := range programs {
-		if q.ToClose() == true {
-			q.Quit() // will be quitting the other open Chrome Windows
+	ctxCh <- ctx
+	// s.appendWindowList(ChromeWindow{programstate})
+	s.appendChromeList(ChromeWindow{programstate})
+	return nil
+}
+
+// Layout has the x , y coordinates of top left corner and width and height of window
+type Layout struct {
+	x, y uint32
+	w, h uint16
+}
+
+// WindowsLayout stores window size and position
+func WindowsLayout(screenInfo *xproto.ScreenInfo, n int) (chromeLayouts []Layout, inputwindow Layout) {
+	heightScreen := 0.8 * float64(screenInfo.HeightInPixels)
+	widthScreen := screenInfo.WidthInPixels
+	inputwindow.h, inputwindow.w = uint16(0.2*float64(screenInfo.HeightInPixels)), uint16(widthScreen)
+	inputwindow.y = uint32(heightScreen)
+
+	rows := int(n/4) + 1
+	columns := uint16(math.Ceil(float64(n / rows)))
+
+	var temp Layout
+	temp.h = uint16(int(heightScreen) / rows)
+	temp.w = uint16(widthScreen / columns)
+
+	for i, r := 0, rows; r > 0; r-- {
+		temp.y = uint32(uint16(r-1) * temp.h)
+		fmt.Println(temp.y)
+		for c := columns; c > 0 && i < n; c-- {
+			temp.x = uint32((c - 1) * temp.w)
+			fmt.Println(temp.x)
+			chromeLayouts = append(chromeLayouts, temp)
+			i++
 		}
 	}
+	return chromeLayouts, inputwindow
 }
 
 // Setup opens all windows and establishes connection with the x server
-func Setup(n int, ctxCh chan context.Context) (*xgb.Conn, xproto.Window, error) {
+func Setup(n int, ctxCh chan context.Context) (*Session, error) {
 	debuggingport := 9222
 	var displayString string
-
 	var session Session
+
+	// calls ForceQuit in case ChromeWindows are closed
 	cmdErrorHandler := func(p *command.ProgramState, err error) error {
 		if err != nil {
 			fmt.Println("returned error %s, calling force quit", err.Error())
@@ -134,41 +144,43 @@ func Setup(n int, ctxCh chan context.Context) (*xgb.Conn, xproto.Window, error) 
 		return err
 	}
 
-	if runtime.GOOS != "darwin" {
+	/*if runtime.GOOS != "darwin" {
 		displayNumber := 1000 + rand.Intn(9999-1000+1)
 		displayString := fmt.Sprintf(":%d", displayNumber)
 		var xephyrLayout Layout
 		xephyrLayout.h, xephyrLayout.w = DefaultXephyrSize()
 		if err := session.CreateXephyrWindow(xephyrLayout, n, cmdErrorHandler); err != nil {
-			return nil, 0, err
+			return nil, err
 		}
-	}
+	}*/
 
-	X, screenInfo, err := Newconn(displayString)
+	screenInfo, err := session.Newconn(displayString)
 	if err != nil {
-		return nil, 0, err
+		return nil, err
 	}
 
 	chromeLayouts, inputWindowLayout := WindowsLayout(screenInfo, n)
-
 	for i := 1; i <= n; i++ {
 		cmd := ChromeCommand(chromeLayouts[i-1], fmt.Sprintf("%s/.aso_sxs_viewer/profiles/dir%d", os.Getenv("HOME"), i), displayString, debuggingport+i)
 		go session.CreateChromeWindow(cmd, ctxCh, cmdErrorHandler)
 	}
-
-	return session.CreateInputWindow(inputWindowLayout, X, screenInfo)
+	if err := session.CreateInputWindow(inputWindowLayout, session.X, screenInfo); err != nil {
+		return nil, err
+	}
+	return &session, nil
 }
 
-func Newconn(displayString string) (*xgb.Conn, *xproto.ScreenInfo, error) {
-	X, err := xgb.NewConnDisplay(displayString)
+func (s *Session) Newconn(displayString string) (*xproto.ScreenInfo, error) {
+	var err error
+	s.X, err = xgb.NewConnDisplay(displayString)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	setup := xproto.Setup(X)
-	screenInfo := setup.DefaultScreen(X)
+	setup := xproto.Setup(s.X)
+	screenInfo := setup.DefaultScreen(s.X)
 
-	return X, screenInfo, nil
+	return screenInfo, nil
 }
 
 func establishChromeConnection(programState *command.ProgramState, timeout int) (context.Context, error) {
@@ -183,59 +195,23 @@ func establishChromeConnection(programState *command.ProgramState, timeout int) 
 
 	allocatorContext, _ := chromedp.NewRemoteAllocator(context.Background(), wsURL)
 
-	// create context
 	ctx, _ := chromedp.NewContext(allocatorContext)
 
 	return ctx, nil
 }
 
-func DefaultXephyrSize() (height, width uint16) {
-	height, width = 900, 1600 // Sensible defaults in case the below fails.
+// ForceQuit closes all open windows
+func (s *Session) ForceQuit() {
+	programs := s.getChromeList()
 
-	X, err := xgb.NewConn()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if err := randr.Init(X); err != nil {
-		log.Println(err)
-		return
-	}
+	fmt.Println("starting force quit")
 
-	screens, err := randr.GetScreenResourcesCurrent(X, xproto.Setup(X).DefaultScreen(X).Root).Reply()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	crtc, err := randr.GetCrtcInfo(X, screens.Crtcs[0], xproto.TimeCurrentTime).Reply()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	return uint16(0.8 * float64(crtc.Height)), uint16(0.8 * float64(crtc.Width))
-}
-
-// WindowsLayout stores window size and position
-func WindowsLayout(screenInfo *xproto.ScreenInfo, n int) (chromeLayouts []Layout, inputwindow Layout) {
-	heightScreen, widthScreen := 0.8*screenInfo.HeightInPixels, screenInfo.WidthInPixels
-	inputwindow.h, inputwindow.w = uint16(0.2*screenInfo.HeightInPixels), uint16(widthScreen)
-	inputwindow.y = uint32(heightScreen)
-
-	rows := int(n/4) + 1
-	columns := math.Ceil(n / rows)
-
-	var temp Layout
-	temp.h = uint16(heightScreen / rows)
-	temp.w = uint16(widthScreen / columns)
-
-	for i, r := 0, rows; r > 0; r-- {
-		temp.y = r * temp.h
-		for c := columns; c > 0 && i < n; c-- {
-			temp.x = c * temp.w
-			chromeLayouts = append(chromeLayouts, temp)
-			i++
+	for _, q := range programs {
+		if q.ToClose() == true {
+			q.Quit() // will be quitting the other open Chrome Windows
 		}
 	}
-	return chromeLayouts, inputwindow
+	if s.InputWin.ToClose() == true {
+		s.InputWin.Quit()
+	}
 }
